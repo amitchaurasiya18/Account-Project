74ac9875b276b541654d27d157b8307a83c06f80
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-16 17:22:19 +0530

    Initial Commit

---
+++ Account.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountProject
+{
+    internal class Account
+    {
+        private int _accountNumber;
+        private string _accountHolderName;
+        private string _bankName;
+        private double _accountBalance;
+        private long _aadharNumber;
+        public static double MAXIMUM_BALANCE = 0;
+        public const double MIN_BALANCE = 500;
+        public int maxBalanceAccountNumber;
+
+
+        //public Account(int accountNumber, string bankName, string accountHolderName) : this(accountNumber, bankName, accountHolderName, 0)
+        //{
+
+        //}
+
+        public Account(int accountNumber, string bankName, string accountHolderName, long aadharNumber) : this(accountNumber, bankName, accountHolderName, aadharNumber,MIN_BALANCE)
+        {
+            
+        }
+
+        public Account(int accountNumber, string bankName, string accountHolderName, long aadharNumber, double accountBalance)
+        {
+            _accountNumber = accountNumber;
+            _accountHolderName = accountHolderName;
+            _bankName = bankName;
+            _aadharNumber = aadharNumber;
+            if (accountBalance < MIN_BALANCE)
+                _accountBalance = MIN_BALANCE;
+            else
+                _accountBalance = accountBalance;
+        }
+
+
+
+        public string DepositAmount(double amount)
+        {
+            _accountBalance += amount;
+            return $"{amount} deposited successfully";
+        }
+
+        public string WithdrawAmount(double amount)
+        {
+            if ((_accountBalance - amount) < MIN_BALANCE)
+                return "No withdrawl possilbe";
+            _accountBalance -= amount;
+            return $"{amount} is withdrawn.";
+        }
+
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine($"Account Number : {_accountNumber}");
+            Console.WriteLine($"Account Holder Name : {_accountHolderName}");
+            Console.WriteLine($"Account Balance : {_accountBalance}");
+            Console.WriteLine($"Aadhar Number : {_aadharNumber}");
+            Console.WriteLine();
+        }
+
+        public void CheckAccountBalance()
+        {
+            Console.WriteLine($"The account balance for {_accountNumber} is {_accountBalance}\n");
+        }
+
+        public int GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public double ReturnMaxBalance(Account[] accounts)
+        {
+            foreach (Account account in accounts)
+            {
+                if (account._accountBalance > MAXIMUM_BALANCE)
+                {
+                    MAXIMUM_BALANCE = account._accountBalance;
+                }
+            }
+            return MAXIMUM_BALANCE;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using AccountProject;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Account[] accounts = new Account[6]
+        {
+            new Account(101, "SBI Bank", "Amit", 123456789101 , 400),
+            new Account(102, "ICICI Bank", "Harsh", 901234567654, 700),
+            new Account(103, "Indian Bank", "Ram", 234567548954),
+            new Account(104, "SBI Bank", "Sakshi",231278562678, 1000),
+            new Account(104, "Indian Bank", "Anushka",239876572678, 100000),
+            new Account(104, "ICICI Bank", "Virat",234455335578)
+        };
+
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.Write("Enter the account number to perform transactions (or enter 0 to exit): ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            if (accountNumber == 0)
+            {
+                continueProgram = false;
+                break;
+            }
+
+            Account selectedAccount = null;
+
+            foreach (Account account in accounts)
+            {
+                if (account.GetAccountNumber() == accountNumber)
+                {
+                    selectedAccount = account;
+                    break;
+                }
+            }
+
+            if (selectedAccount != null)
+            {
+                bool continueTransactions = true;
+                while (continueTransactions)
+                {
+                    Console.WriteLine("\n------Select transaction:------\nPress 1 for Check Balance\nPress 2 for Deposit\nPress 3 for Withdrawl\nPress 4 for Account Details\nPress 5 to get details of account with maximum balance\nPress 6 to Exit");
+                    int selectTransaction = int.Parse(Console.ReadLine());
+
+                    switch (selectTransaction)
+                    {
+                        case 1:
+                            selectedAccount.CheckAccountBalance();
+                            break;
+                        case 2:
+                            Console.Write("Enter the amount which you want to deposit : ");
+                            double depositAmount = double.Parse(Console.ReadLine());
+                            Console.WriteLine(selectedAccount.DepositAmount(depositAmount));
+                            break;
+                        case 3:
+                            Console.Write("Enter the amount which you want to Withdraw : ");
+                            double withdrawAmount = double.Parse(Console.ReadLine());
+                            Console.WriteLine(selectedAccount.WithdrawAmount(withdrawAmount));
+                            break;
+                        case 4:
+                            selectedAccount.PrintAccountDetails();
+                            break;
+                        case 5:
+                            Console.WriteLine($"Maximum Balance is : {selectedAccount.ReturnMaxBalance(accounts)}");
+                            break;
+                        case 6:
+                            continueTransactions = false;
+                            break;
+                        default:
+                            Console.WriteLine("Select Transaction again...");
+                            break;
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Account not found. Please try again.");
+        }
+    }
+}
